2725ea984768f44493c9429ee76765817fdcd2da
Prassanna <iamprassanna@gmail.com>
2021-07-11 09:57:16 +0530

    Delete factoryMethod directory

--- factoryMethod/index.js
+++
@@ -1,9 +0,0 @@
-var AccountFactory = require('./accountFactory');
-// var Machine = require('./Machine');
-
-// const user = new AccountFactory('Human');
-
-const machine = new AccountFactory('NonHuman');
-
-// console.log('USER > ', user.toString())
-console.log('MACHINE > ', machine.toString())
